9bddbbf1b2d0b044be759561a4ec0eba86cdad52
kimsanglok <evertree6031@gmail.com>
2023-03-15 19:11:19 +0900

    chore: construct basic skeleton of file structure

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,5 @@
-import { useState } from "react";
-
 const HomePage = () => {
-  const [list, setList] = useState<number[]>([]);
-
-  const handleClick = () => {
-    setList((prev) => [...prev, new Date().getTime()]);
-  };
-
-  return (
-    <div>
-      <div>click the button</div>
-      <button onClick={handleClick}>click</button>
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div>Welcome</div>;
 };
 
 export default HomePage;
